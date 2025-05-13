abd4709cb55125af444a2dbec9f591e4cc4ff328
Remy <remy.currau@gmail.com>
2025-05-13 23:34:06 +0200

    add_clean_minimalist_graph

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import chess.pgn
 from stockfish import Stockfish
 import io
 import pandas as pd
+import plotly.graph_objects as go
+
 
 stockfish_path = "C:\Program Files (x86)\stockfish\stockfish-windows-x86-64-avx2.exe"
 stockfish = Stockfish(path=stockfish_path, depth=15)
@@ -64,6 +66,64 @@ if pgn_text:
 
     # Afficher l'histogramme
     st.header("Évolution de l'avantage")
-    fig = plot_evaluation_histogram(analysis)
-    st.plotly_chart(fig)
+    evals = [coup["eval"] for coup in analysis]
+    min_val = min(evals) - 100
+
+    fig = go.Figure()
+
+    # Fond blanc jusqu'en bas
+    fig.add_trace(go.Scatter(
+        x=list(range(len(evals))),
+        y=[min_val]*len(evals),
+        mode='lines',
+        line=dict(color='white'),
+        fill=None,
+        showlegend=False,
+        hoverinfo="skip"
+        )
+    )
+
+    # Courbe d'évaluation + remplissage
+    fig.add_trace(go.Scatter(
+        x=list(range(len(evals))),
+        y=evals,
+        mode='lines',
+        line=dict(color='white'),
+        fill='tonexty',
+        fillcolor='white',
+        showlegend=False,
+        hoverinfo="skip"
+    ))
     
+    # Ligne y=0 au premier plan
+    fig.add_shape(
+    type="line",
+    x0=0,
+    y0=0,
+    x1=len(evals)-1,
+    y1=0,
+    line=dict(color="gray", width=1),
+    layer="above"
+    )
+
+
+    fig.update_layout(
+        height=80,
+        margin=dict(l=0, r=0, t=0, b=0),
+        dragmode=False,
+        xaxis=dict(
+            showgrid=False,
+            showticklabels=False,
+            zeroline=False,
+            showline=False,
+        ),
+        yaxis=dict(
+            showgrid=False,
+            showticklabels=False,
+            zeroline=False,
+            showline=False,
+        )
+    )   
+    
+
+    st.plotly_chart(fig,use_container_width=True,config={'displayModeBar': False,"staticPlot": True})
