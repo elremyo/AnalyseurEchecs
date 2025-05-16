99c86c0d4e876d0366f4e8f1b933ec08e2352014
elremyo <remy.currau@gmail.com>
2025-05-16 10:50:19 +0200

    hide streamlit elements

--- app.py
+++ app.py
@@ -8,6 +8,40 @@ import os
 st.set_page_config(layout="wide")
 
 
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
 #os.system("chmod +x ./bin/stockfish")
 
 #stockfish_path = "./bin/stockfish"
